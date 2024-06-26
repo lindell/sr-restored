90e846048e073e446218db0bae7924d83048b404
Johan Lindell <johan@lindell.me>
2024-06-26 11:03:18 +0000

    Add more bucket to rss get metric

--- podcast/podcast.go
+++ podcast/podcast.go
@@ -39,7 +39,7 @@ var (
 	rssGetSecondsMetric = promauto.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "rss_get_seconds",
 		Help:    "Time to get an RSS feed",
-		Buckets: []float64{0.1, 1, 5, 10},
+		Buckets: []float64{0.1, 1, 5, 10, 20, 30},
 	}, []string{"cached"})
 	rssGetTotalMetric = promauto.NewCounterVec(prometheus.CounterOpts{
 		Name: "rss_get_total",
