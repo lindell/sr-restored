a0f547bd725f6e8c63c0479ccad48fbff2c99de8
Johan Lindell <johan@lindell.me>
2026-04-17 21:19:10 +0200

    Update Playwright config to specify server host and timeout settings

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -2,8 +2,10 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		command: 'npm run build && npm run preview -- --host 127.0.0.1',
+		url: 'http://127.0.0.1:4173',
+		reuseExistingServer: !process.env.CI,
+		timeout: 120000
 	},
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/
