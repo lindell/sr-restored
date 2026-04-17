99febc6e0cec202bd7a470605cc8401fc2a53b99
Johan Lindell <johan@lindell.me>
2026-04-17 21:59:57 +0200

    x

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,12 +1,17 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-	webServer: {
-		command: 'node test-server.mjs',
-		port: 4173
-	},
+	webServer: process.env.CI
+		? undefined
+		: {
+				command: 'node test-server.mjs',
+				port: 4173
+			},
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	use: {
+		baseURL: 'http://127.0.0.1:4173'
+	}
 };
 
 export default config;
